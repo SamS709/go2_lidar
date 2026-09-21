81f0d05d4a627c90107e23706ba0cfed53614392
Sami <sami.leroux75@gmail.com>
2026-09-21 11:17:22 +0200

    ready to train with command: python scripts/rsl_rl/train.py --task Isaac-Velocity-Rough-Go2-CNN-RNN-Lidar-Direct-v0 --headless

--- source/go2_lidar/go2_lidar/tasks/direct/go2_lidar/go2_lidar_env.py
+++ source/go2_lidar/go2_lidar/tasks/direct/go2_lidar/go2_lidar_env.py
@@ -241,8 +241,8 @@ class Go2LidarEnv(DirectRLEnv):
         
 
     def _apply_action(self):
-        # self._robot.set_joint_position_target(self._processed_actions)
-        self._robot.set_joint_position_target(self._robot.data.default_joint_pos)    
+        self._robot.set_joint_position_target(self._processed_actions)
+        # self._robot.set_joint_position_target(self._robot.data.default_joint_pos)    
         
     def _compute_height_data(self, method, randomize: bool = False):
         if method == "normal":
